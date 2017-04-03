8ca1436adde0e862ceb6b7a9e2dc24e4a007981f
Dreadknotts <vanputten.jj@gmail.com>
2017-04-03 16:11:08 -0400

    VisPop Update
    Added live update animations, country toggle, css tweaks, about page,
    main nav, title, button toggle, and ajax function.

--- projects/visPop/js/index.js
+++ projects/visPop/js/index.js
@@ -1,12 +1,21 @@
 var countryList = [
     "World",
-    "Bangladesh",
+    "ASIA",
+    "OCEANIA",
+    "EUROPE",
+    "NORTHERN AMERICA",
+    "Central America",
+    "South America",
+    "Australia",
+    "Caribbean",
     "Brazil",
+    "Canada",
     "China",
     "India",
     "Indonesia",
     "Japan",
     "Nigeria",
+    "Mexico",
     "Pakistan",
     "Russian Federation",
     "United States"
@@ -15,31 +24,27 @@ var countryList = [
 // ---------------------
 // Request Data
 // ---------------------
-var json = (function () {
-    var json = null;
-    var country = countryList[0]
+var jsonInit = (function () {
+    var jsonInit = null;
+    var country = countryList[0];
     // JQuery Ajax Request Documentation: https://api.jquery.com/jquery.ajax/
     $.ajax({
         'async': false,
         'global': false,
-        'url': 'https://api.population.io/1.0/population/' + country + '/today-and-tomorrow/?format=json',
+        'url': 'http://api.population.io/1.0/population/' + country + '/today-and-tomorrow/?format=json',
         'dataType': 'json',
         'success': function (data) {
-          json = data
+          jsonInit = data
         },
         'error': function(jqXHR, textStatus, errorThrown) {
           return $("#population").text('Sorry!  There was an error requesting your data.')
         }
     });
-    return json
+    return jsonInit
 })();
-
-// JQuery For Loop
-var pop = json.total_population;
-// $.each(pop, function(i, object) {
-//   $('#population').append('<span>' + pop[i].population + '</span>');
-// });
-
+var count = 1;
+var country = countryList[0];
+var pop = jsonInit.total_population;
 var popNumber = pop[0].population;
 var dotTotal = Math.round(popNumber / 10000000);
 var dotNumber = 0;
@@ -47,25 +52,54 @@ var dotNumber = 0;
 $(function init() {
 
 dotBuildToday();
+dotBuildBG();
 
-  $('#population').append('<span>' + popNumber + '</span>');
-
+  $('#country').text( country )
+  $('#population').append('<span></span>');
   $("#day").click(dayClick);
+  $("#country").click(ajaxUpdate);
+});
 
-  });
+function dotBuildBG() {
+  dotNumber = 0;
+  while (dotNumber < dotTotal){
+    $(".bgVis").append('<li class="bgDot popBG" id="dotbg"></li>')
+      dotNumber++;
+  }
+}
 
-function dotBuildToday () {
+function dotBuildToday() {
   while (dotNumber < dotTotal) {
-      $(".popVis").append('<li class="popDot popToday" id="dot"></li>');
+    var $popVis = $('<li class="popDot popToday" id="dot" style="opacity: 0;"></li>')
+      $(".popVis").append($popVis);
          dotNumber++;
     };
+
+    $('#visInfo').children('li').each(function(i){
+      var $item = $(this);
+      setTimeout(function() {
+      $item.animate({"opacity": 1}, 60);
+    }, 30*i);
+});
+
+  $('#population').text( popNumber.toLocaleString('en'));
 };
 
-function dotBuildTomorrow () {
+function dotBuildTomorrow() {
   while (dotNumber < dotTotal) {
-      $(".popVis").append('<li class="popDot popTomorrow" id="dot"></li>');
+    var $popVis = $('<li class="popDot popTomorrow" id="dot" style="opacity: 0;"></li>')
+      $(".popVis").append($popVis);
          dotNumber++;
     };
+
+    $('#visInfo').children('li').each(function(i){
+      var $item = $(this);
+      setTimeout(function() {
+      $item.animate({"opacity": 1}, 40);
+    }, 20*i);
+});
+
+  $('#population').text( popNumber.toLocaleString('en'));
 };
 
 function dayClick() {
@@ -74,20 +108,60 @@ function dayClick() {
     $this.toggleClass('tomorrow');
     if($this.hasClass('tomorrow')){
       $this.text('Tomorrow');
+      $(".header-subtitle").text('Country Population Tomorrow');
        popNumber = pop[1].population;
        dotNumber = 0;
 
-      $('#population').text( popNumber );
       $("li").remove('#dot');
       dotBuildTomorrow();
 
     } else {
       $this.text('Today');
+      $(".header-subtitle").text('Country Population Today');
       popNumber = pop[0].population;
       dotNumber = 0;
 
-     $('#population').text( popNumber );
      $("li").remove('#dot');
      dotBuildToday();
+    }
+};
+
+function ajaxUpdate() {
+
+  var jsonUpdate = (function () {
+      var jsonUpdate = null;
+      country = countryList[count];
+      // JQuery Ajax Request Documentation: https://api.jquery.com/jquery.ajax/
+      $.ajax({
+          'async': false,
+          'global': false,
+          'url': 'http://api.population.io/1.0/population/' + country + '/today-and-tomorrow/?format=json',
+          'dataType': 'json',
+          'success': function (data) {
+            jsonUpdate = data
+          },
+          'error': function(jqXHR, textStatus, errorThrown) {
+            return $("#population").text('Sorry!  There was an error requesting your data.')
+          }
+      });
+      $('#country').text( country );
+      if(count < 19){ count++; } else { count = 0; };
+      return jsonUpdate;
+  })();
+
+  pop = jsonUpdate.total_population;
+  dotNumber = 0;
+
+  var dayBtn = $("#day")
+  if(dayBtn.hasClass('tomorrow')){
+    popNumber = pop[1].population;
+    dotTotal = Math.round(popNumber / 10000000);
+    $("li").remove('#dot');
+    dotBuildTomorrow();
+    } else {
+      popNumber = pop[0].population;
+      dotTotal = Math.round(popNumber / 10000000);
+      $("li").remove('#dot');
+      dotBuildToday();
     }
 };
